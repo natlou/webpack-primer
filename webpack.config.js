a3744b46386bbf73303c1ba49a04b49d141e68e5
natlou <natloudev@gmail.com>
2022-07-13 13:32:58 -0700

    configured loader for scss'

--- webpack.config.js
+++ webpack.config.js
@@ -9,11 +9,19 @@ module.exports = {
     // the first file it looks at and compiles, but it will compile and import dependencies
     // first file in the tree.
     module: {
-        rules: [ // rule to transpile typescipt into javascript
-            {
+        rules: [ 
+            { // rule to transpile typescipt into javascript
                 test: /\.ts$/, //needs to pass test to transpile (checks if it is typescipt file) $ checks if .ts is at the end of the file
                 use: 'ts-loader', // typescript transpiler
                 include: [path.resolve(__dirname, 'src')] // only use files from source folder
+            },
+            {
+                test:/\.scss$/, // regex for scss
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
             }
         ]
     },
